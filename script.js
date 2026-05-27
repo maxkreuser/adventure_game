b409830657251fa117dec1b60f5d582c27886a99
Maximilian Kreuser <mkreuser@rrc.ca>
2026-05-27 10:37:20 -0500

    feat: create complete story line inside the ADVENTURE_PLAN.md file. Initiated the database structure in JS file.

---
+++ script.js
@@ -0,0 +1,23 @@
+const gameScript = [
+    {
+        path: 1,
+        line: 1,
+        text:"You fell asleep studying. You wake up. No one is in the library."
+    },
+    {
+        path: 1,
+        line: 2,
+        text:"It's night, time to leave. Do you take the stair well or the elevator?",
+        answer: ["stair well", "elevator"]
+    },
+    {
+        path: 1.1,
+        line:1,
+        text:"The stair well has no light except for a small emergency blinking red light.",
+    },
+        {
+        path: 1.1,
+        line:2,
+        text:"You exit into the atrium. No one is there, not even the guards.",
+    },
+]
